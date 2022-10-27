de75de68e6991ea11708639968733303a9cc61b1
Samyak Gantayat <samyakgantayat123@gmail.com>
2022-10-27 22:06:23 +0530

    Update GCP.cpp

--- GCP.cpp
+++ GCP.cpp
@@ -7,7 +7,12 @@ int main()
     printf("Please Enter two integer Values \n");
     scanf("%d %d", &Num1, &Num2);
 
-   
+    for(i = 1; i <= Num1 && i <= Num2; i++)
+    {
+        if(Num1 % i == 0 && Num2 % i == 0)
+            GCD = i;
+    }
+
     printf("GCD of %d and %d is = %d", Num1, Num2, GCD);
     return 0;
 }
